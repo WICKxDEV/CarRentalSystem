a8fcb0ea8c1ed514a4c0dbd3632d1da83f48f54f
WICKxDEV <160169165+WICKxDEV@users.noreply.github.com>
2024-05-22 14:56:43 +0530

    created car class

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.Arraylist;
+import java.util.List;
+import java.util.Scanner;
+
+
+class Car {
+    
+    private String carId;
+
+    private String brand;
+
+    private String model;
+
+    private double basePricePerDay;
+
+    private boolean isAvailable;
+
+    public Car(String carId, String brand, String model, double basePricePerDay){
+        this.carId = carId;
+        this.brand = brand;
+        this.model = model;
+        this.basePricePerDay = basePricePerDay;
+        this.isAvailable = true;
+    }
+
+}
+
+
+class Customer {...}
+
+
+class Renteal {...}
+
+
+class CarRentalSystem {...}
+
+public class Main{
+    public static voide main(string[] args) {...}
+}
